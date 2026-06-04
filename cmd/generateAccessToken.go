84b4ccc541a03481a3b9e33655afc1ab372d4fe1
tsenv8 <tsantos@enov8tive.com>
2026-06-04 17:03:49 +0800

    fix: generateAccessToken

--- cmd/generateAccessToken.go
+++ cmd/generateAccessToken.go
@@ -4,6 +4,7 @@ import (
 	"abctlx/internal/airbyte"
 	"abctlx/internal/config"
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 )
@@ -11,7 +12,11 @@ import (
 var generateAccessTokenCmd = &cobra.Command{
 	Use: "token",
 	Run: func(cmd *cobra.Command, args []string) {
-		output := airbyte.New(config.Data).GenerateAccessToken()
+		output, err := airbyte.New(config.Data).GenerateAccessToken()
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		fmt.Println(output)
 	},
 }
