16b2a969f0d41fee5b08be4cd52d7483a957619f
tsenv8 <tsantos@enov8tive.com>
2026-06-04 17:00:56 +0800

    update: added types and constants

---
+++ internal/airbyte/const.go
@@ -0,0 +1,15 @@
+package airbyte
+
+// API
+const HEADER_CONTENT_TYPE = "application/json"
+const REQUEST_FAIL = "API Request Failed"
+const REQUEST_SUCCESS = "API Request Successful"
+const JSON_MARSHAL_FAIL = "JSON Marshal Fail"
+const JSON_UNMARSHAL_FAIL = "JSON Unmarshal Fail"
+
+// API ENDPOINTS
+const GENERATE_ACCESS_TOKEN_ENDPOINT = "/applications/token"
+const HEALTH_CHECK_ENDPOINT = "/v1/health"
+const LIST_SOURCES_ENDPOINT = "/v1/sources"
+const LIST_WORKSPACES_ENDPOINT = "/v1/workspaces"
+const CREATE_SOURCE_ENDPOINT = "/v1/sources"
---
+++ internal/airbyte/types.go
@@ -0,0 +1,73 @@
+package airbyte
+
+type AbctlxResponse struct {
+	msg  string
+	body []byte
+	data any
+}
+
+// sources
+type CreateSourceRequest struct {
+	Name          string                         `json:"name"`
+	WorkspaceId   string                         `json:"workspaceId"`
+	Configuration PostgresConfigurationParameter `json:"configuration"`
+}
+
+type CreateSourceParams struct {
+	name            string
+	hostName        string
+	dbName          string
+	username        string
+	password        string
+	replicationSlot string
+	publicationName string
+	schemas         []string
+	port            int
+}
+
+type PostgresConfigurationParameter struct {
+	SourceType        string                        `json:"sourceType"`
+	Host              string                        `json:"host"`
+	Port              int                           `json:"port"`
+	Database          string                        `json:"database"`
+	Schemas           []string                      `json:"schemas"`
+	Username          string                        `json:"username"`
+	Password          string                        `json:"password"`
+	SSlMode           *SSLModeParameter             `json:"ssl_mode"`
+	ReplicationMethod CDCReplicationMethodParameter `json:"replication_method"`
+	TunnelMethod      TunnelMethodParameter         `json:"tunnel_method"`
+}
+
+type CDCReplicationMethodParameter struct {
+	Method          string `json:"method"`
+	Plugin          string `json:"plugin"`
+	ReplicationSlot string `json:"replication_slot"`
+	Publication     string `json:"publication"`
+}
+
+type SSLModeParameter struct {
+	Mode string `json:"mode"`
+}
+
+type TunnelMethodParameter struct {
+	TunnelMethod string `json:"tunnel_method"`
+}
+
+// applications
+type GenerateAccessTokenRequest struct {
+	ClientId  string `json:"client_id"`
+	ClientKey string `json:"client_secret"`
+}
+
+// workspaces
+type ListWorkspacesResponse struct {
+	Next     string
+	Previous string
+	Data     []WorkspaceData
+}
+
+type WorkspaceData struct {
+	WorkspaceId   string
+	Name          string
+	DataResidency string
+}
