6f0da667500a7da870a1d85163df0742dcb026d3
tsenv8 <tsantos@enov8tive.com>
2026-06-09 14:24:17 +0800

    refactor: clean up AirbyteClient interface and update API endpoint constants

--- internal/airbyte/types.go
+++ internal/airbyte/types.go
@@ -8,6 +8,123 @@ type AbctlxResponse struct {
 	Status   int
 }
 
+// connections
+type ConnectionData struct {
+	ConnectionId                     string                      `json:"connectionId"`
+	Name                             string                      `json:"name"`
+	SourceId                         string                      `json:"sourceId"`
+	DestinationId                    string                      `json:"destinationId"`
+	WorkspaceId                      string                      `json:"workspaceId"`
+	Status                           string                      `json:"status"`
+	Schedule                         ConnectionScheduleParameter `json:"schedule"`
+	DataResidency                    string                      `json:"dataResidency"`
+	NonBreakingSchemaUpdatesBehavior string                      `json:"nonBreakingSchemaUpdatesBehavior"`
+	NamespaceDefinition              string                      `json:"namespaceDefinition"`
+	NamespaceFormat                  string                      `json:"namespaceFormat"`
+	Prefix                           string                      `json:"prefix"`
+	Configurations                   StreamConfigurations        `json:"configurations"`
+	CreatedAt                        int                         `json:"createdAt"`
+}
+type UpdateConnectionRequest struct {
+	Name                             string                      `json:"name,omitempty"`
+	Configurations                   StreamConfigurations        `json:"configurations"`
+	Schedule                         ConnectionScheduleParameter `json:"schedule"`
+	DataResidency                    string                      `json:"dataResidency,omitempty"`
+	NamespaceDefinition              string                      `json:"namespaceDefinition,omitempty"`
+	NamespaceFormat                  string                      `json:"namespaceFormat,omitempty"`
+	Prefix                           string                      `json:"prefix,omitempty"`
+	NonBreakingSchemaUpdatesBehavior string                      `json:"nonBreakingSchemaUpdatesBehavior,omitempty"`
+	Status                           string                      `json:"status,omitempty"`
+}
+
+type CreateConnectionRequest struct {
+	Name                             string                      `json:"name"`
+	SourceId                         string                      `json:"sourceId"`
+	Configurations                   StreamConfigurations        `json:"configurations"`
+	DestinationId                    string                      `json:"destinationId"`
+	Schedule                         ConnectionScheduleParameter `json:"schedule"`
+	Residency                        string                      `json:"residency"`
+	NamespaceDefinition              string                      `json:"namespaceDefinition"`
+	Prefix                           string                      `json:"prefix,omitempty"`
+	NonBreakingSchemaUpdatesBehavior string                      `json:"nonBreakingSchemaUpdatesBehavior,omitempty"`
+	Status                           string                      `json:"status"`
+}
+
+type ListConnectionResponse struct {
+	Previous string           `json:"previous"`
+	Next     string           `json:"next"`
+	Data     []ConnectionData `json:"data"`
+}
+
+type StreamConfigurations struct {
+	streams []StreamConfiguration
+}
+
+type StreamConfiguration struct {
+	Name           string   `json:"name"`
+	SyncMode       string   `json:"syncMode"`
+	CursorField    []string `json:"cursorField"`
+	SelectedFields []string `json:"selectedFields"`
+}
+type ConnectionScheduleParameter struct {
+	Type    string `json:"scheduleType"`
+	CronExp string `json:"cronExpression"`
+}
+
+// destinations
+
+type CreateDestinationFlags struct {
+	Name       string
+	ConfigType string
+}
+
+type UpdateDestinationFlags struct {
+	DestName     *string
+	Name         *string
+	ConfigType   *string
+	Host         *string
+	Port         *int
+	Database     *string
+	Username     *string
+	Password     *string
+	TunnelMethod *string
+}
+
+type CreateDestinationRequest struct {
+	Name          string                            `json:"name"`
+	WorkspaceId   string                            `json:"workspaceId"`
+	Configuration DestinationConfigurationParameter `json:"configuration"`
+}
+
+type DestinationConfigurationParameter struct {
+	Host            string                `json:"host,omitempty"`
+	Port            int                   `json:"port,omitempty"`
+	Database        string                `json:"database,omitempty"`
+	Username        string                `json:"username,omitempty"`
+	Password        string                `json:"password,omitempty"`
+	TunnelMethod    TunnelMethodParameter `json:"tunnel_method,omitempty"`
+	DestinationType string                `json:"destinationType,omitempty"`
+}
+
+type UpdateDestinationRequest struct {
+	Name          string                            `json:"name,omitempty"`
+	Configuration DestinationConfigurationParameter `json:"configuration"`
+}
+
+type ListDestinationResponse struct {
+	Previous string
+	Next     string
+	Data     []DestinationData
+}
+
+type DestinationData struct {
+	DestinationId   string `json:"destinationId"`
+	Name            string `json:"name"`
+	DestinationType string `json:"destinationType"`
+	WorkspaceId     string `json:"workspaceId"`
+	Configuration   any    `json:"configuration,omitempty"`
+}
+
 // sources
 type CreateSourceRequest struct {
 	Name          string                         `json:"name"`
@@ -62,12 +179,12 @@ type CreateSourceParams struct {
 }
 
 type ListSourcesResponse struct {
-	Next     string            `json:"next"`
-	Previous string            `json:"previous"`
-	Data     []ListSourcesData `json:"data"`
+	Next     string       `json:"next"`
+	Previous string       `json:"previous"`
+	Data     []SourceData `json:"data"`
 }
 
-type ListSourcesData struct {
+type SourceData struct {
 	SourceId    string `json:"sourceId"`
 	Name        string `json:"name"`
 	SourceType  string `json:"sourceType"`
