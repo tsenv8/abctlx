4e18b7f05789c275c29b9257c152e45b5d26584a
tsenv8 <tsantos@enov8tive.com>
2026-06-16 10:38:23 +0800

    feat: add scripts for source management and connection resource updates

---
+++ scripts/update-connection-resources.sh
@@ -0,0 +1,14 @@
+./abctlx res update-connection \
+    --id="" \
+    --max_cpu=8 \
+    --min_cpu=8 \
+    --max_mem=8 \
+    --min_mem=8 
+
+
+#   -h, --help          help for update-connection
+#       --id string     The Connection ID to update.
+#       --max_cpu int   The Maximum CPU usage
+#       --max_mem int   The Maximum RAM usage
+#       --min_cpu int   The Minimum CPU usage
+#       --min_mem int   The Minimum RAM usage
